7af60918c853b07621517bda0275f8227c69513c
quach-duc-mulodo <quach.duc@mulodo.com>
2015-10-06 14:12:04 +0700

    Add init method for class Student

--- basic-class.php
+++ basic-class.php
@@ -8,8 +8,18 @@
         public $name = 'studnet name';
         public $age = 10;
 
+        // Init method for this class
+        function __construct($name = 'default name', $age = 0) {
+            $this->name = $name;
+            $this->age = $age;
+        }
+
     }
 
 $st = new Student();
 
 var_dump($st->name, $st->age);
+
+$st_mod = new Student('Duc', 12);
+
+var_dump($st_mod->name, $st_mod->age);
