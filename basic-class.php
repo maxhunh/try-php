16f21c43589e1fedd2d15c6e57025e2a86ea6129
quach-duc-mulodo <quach.duc@mulodo.com>
2015-10-06 14:08:21 +0700

    Init basic class Student

---
+++ basic-class.php
@@ -0,0 +1,15 @@
+<?php
+    /**
+    * Basic class student
+    */
+    class Student {
+
+        // Atribute of class
+        public $name = 'studnet name';
+        public $age = 10;
+
+    }
+
+$st = new Student();
+
+var_dump($st->name, $st->age);
