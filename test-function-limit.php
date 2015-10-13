3d2b0eaaac42228a1fa350bb380ba208c4314d7e
quach-duc-mulodo <quach.duc@mulodo.com>
2015-10-13 10:13:39 +0700

    Test private method in extend class

--- test-function-limit.php
+++ test-function-limit.php
@@ -2,14 +2,14 @@
     /**
     * Class Student
     */
-    class Student
+    abstract class Student
     {
 
-        protected $name = '';
-        protected $age = 0;
-        protected $info = [];
+        public $name = '';
+        private $age = 0;
+        private $info = [];
 
-        function __construct($name = null, $age = null, $address = null, $other)
+        function __construct($name = null, $age = 0, $address = null, $other = null)
         {
             $this->name = $name;
             $this->age = $age;
@@ -20,26 +20,58 @@
         public function getStudent()
         {
             echo "\n Name: ".$this->name.
-                 "\n Age: ".$this->age.
-                 "\n Address: ".$this->info['address'].
-                 "\n Other: ".$this->info['other'];
+            "\n Age: ".$this->age.
+            "\n Address: ".$this->info['address'].
+            "\n Other: ".$this->info['other'];
 
             echo $this->getInfo2nd();
         }
 
         private function getInfo2nd()
         {
-            echo "\n Test private method of obj!!!";
+            echo "\n Test private method of obj Student !!!";
         }
 
         private static function getInfo1st()
         {
-            echo "\n Test private method of class!!!";
+            echo "\n Test private method of class Student !!!";
         }
 
+        // Menote: have static is method of class
+        //         private use in class
+        //         public use out class
         public static function getInfoClass()
         {
             Student::getInfo1st();
+        }
+    }
+
+    /**
+    * Class Master
+    */
+    class Master extends Student
+    {
+        private $level = '';
+
+        function __construct($name = null, $age = 0, $address = null, $other = null, $level = null) {
+            parent::__construct($name,$age,$address,$other);
+            $this->level = $level;
+        }
+
+        public function getLevelMaster()
+        {
+            # code...
+            echo "\n Level: ".$this->level;
+        }
+
+        public static function getInfoMaster1st()
+        {
+            echo "\n Test private method of class Master !!!";
+        }
+
+        public function getInfoMaster2nd()
+        {
+            echo "\n Test private method of obj Master !!!";
         }
     }
 
@@ -48,8 +80,11 @@
     /**
      * Test
      */
-
-    $st = new Student('Duc', 23, '123 tran hung dao', 'info other');
+    $st = new Master('Duc', 23, '123 tran hung dao', 'info other','level 12');
+    echo $st->name;
     $st->getStudent();
+    $st->getLevelMaster();
+    $st->getInfoMaster2nd();
 
-    Student::getInfoClass();
+    Master::getInfoClass();
+    Master::getInfoMaster1st();
